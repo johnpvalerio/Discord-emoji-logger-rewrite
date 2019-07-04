e636a0007b4e21561a4cb309a23cb59087f76842
John Paulo Valerio <johnp.valerio@gmail.com>
2019-07-03 22:29:31 -0400

    added last_used as constructor argument, sets none as default

--- EmojiStat.py
+++ EmojiStat.py
@@ -1,6 +1,6 @@
 class EmojiStat:
-    def __init__(self, emoji_obj, instance_count=0, total_count=0):
+    def __init__(self, emoji_obj, instance_count=0, total_count=0, last_used=None):
         self.emoji_obj = emoji_obj  # Discord Emoji object
         self.instance_count = instance_count  # All emote use in a msg = 1 count
         self.total_count = total_count  # All emote use in a msg
-        self.last_used = None  # Datetime last used
+        self.last_used = last_used  # Datetime last used
