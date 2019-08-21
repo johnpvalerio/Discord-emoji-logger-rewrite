79b2d75beb43cc37bd47699be6f4171de203ec79
John Paulo Valerio <johnp.valerio@gmail.com>
2019-08-21 11:15:41 -0400

    Logging added, file handler and stream handler enabled

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import configparser
+import logging
 
 from discord.ext import commands
 
@@ -8,7 +9,18 @@ import EmojiView
 
 
 def main():
-    print('AegisBot Initializing')
+    # logging.basicConfig(filename='configs/bot.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+    logger = logging.getLogger('bot_logs')
+    stm_log = logging.StreamHandler()
+    fh_log = logging.FileHandler("configs/bot.log")
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stm_log.setFormatter(formatter)
+    fh_log.setFormatter(formatter)
+    logger.addHandler(stm_log)
+    logger.addHandler(fh_log)
+    logger.setLevel(logging.INFO)
+
+    logger.info("AegisBot initializing")
     bot = commands.Bot(command_prefix='!')
 
     model = EmojiModel.Model(bot)
